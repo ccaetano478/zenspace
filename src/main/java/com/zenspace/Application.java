35d3bc8265b84a36c368a58e46baad4c56f907d0
Camila Caetano <camilaanjos478@gmail.com>
2026-04-13 16:40:37 -0300

    chore.: change dependencies and java version

--- src/main/java/com/zenspace/Application.java
+++ src/main/java/com/zenspace/Application.java
@@ -2,12 +2,8 @@ package com.zenspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.shell.core.command.annotation.Command;
-import org.springframework.shell.core.command.annotation.Option;
-
 
 @SpringBootApplication
-
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
